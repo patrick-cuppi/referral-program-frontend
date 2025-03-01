eccaa80fe7417c8b1e36731f6bb7eb68c99f624d
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-03-01 16:17:25 -0300

    :sparkles: style: added public and assets files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,3 @@
-import { Button } from '@/components/button'
-import { IconButton } from '@/components/icon-button'
-import { InputField, InputIcon, InputRoot } from '@/components/input'
-import { ArrowRight, Copy, Mail } from 'lucide-react'
-
 export default function Home() {
-  return (
-    <main>
-      <Button type="submit">
-        Enviar
-        <ArrowRight />
-      </Button>
-      <IconButton>
-        <Copy />
-      </IconButton>
-      <div>
-        <InputRoot>
-          <InputIcon>
-            <Mail className="size-5" />
-          </InputIcon>
-
-          <InputField />
-        </InputRoot>
-      </div>
-    </main>
-  )
+  return <main></main>
 }
