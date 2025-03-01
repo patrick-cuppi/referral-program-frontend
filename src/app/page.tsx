97b0591493fded43b839e05ac26015f0bf64fe60
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-03-01 15:27:07 -0300

    :dizzy: style: create input component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/button";
 import { IconButton } from "@/components/icon-button";
-import { Input } from "@/components/input";
-import { ArrowRight, Copy } from "lucide-react";
+import { InputField, InputIcon, InputRoot } from "@/components/input";
+import { ArrowRight, Copy, Mail } from "lucide-react";
 
 export default function Home() {
   return (
@@ -14,7 +14,13 @@ export default function Home() {
         <Copy />
       </IconButton>
       <div>
-        <Input type="email"/>
+        <InputRoot>
+          <InputIcon>
+            <Mail className="size-5" />
+          </InputIcon>
+
+          <InputField />
+        </InputRoot>
       </div>
     </main>
   );
