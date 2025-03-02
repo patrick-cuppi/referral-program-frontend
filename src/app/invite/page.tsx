8bc05e8ac2afbe956c511c27156bc4a4a7397d97
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-03-02 15:12:39 -0300

    :sparkles: feat: ranking card

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -3,6 +3,9 @@ import { InputField, InputIcon, InputRoot } from '@/components/input'
 import { BadgeCheck, Copy, Link, Medal, MousePointerClick } from 'lucide-react'
 import Image from 'next/image'
 import logo from '../../assets/logo.svg'
+import cooper from '../../assets/medal-cooper.svg'
+import gold from '../../assets/medal-gold.svg'
+import silver from '../../assets/medal-silver.svg'
 
 export default function InvitePage() {
   return (
@@ -74,6 +77,56 @@ export default function InvitePage() {
               </span>
               <Medal className="size-5 text-purple absolute top-3 left-3" />
             </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full max-w-[440px] space-y-5">
+        <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
+          Ranking de indicações
+        </h2>
+        <div className="space-y-4">
+          <div className=" relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibolsd">1º | </span>
+              Darth Vader
+            </span>
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              2651
+            </span>
+            <Image
+              src={gold}
+              alt="Primeira posição - medalha de ouro"
+              className="absolute top-0 right-8"
+            />
+          </div>
+          <div className=" relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibolsd">2º | </span>
+              Kylo Ren
+            </span>
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              1930
+            </span>
+            <Image
+              src={silver}
+              alt="Segunda posição - medalha de prata"
+              className="absolute top-0 right-8"
+            />
+          </div>
+          <div className=" relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibolsd">3º | </span>
+              Imperador Palpatine
+            </span>
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              1042
+            </span>
+            <Image
+              src={cooper}
+              alt="Terceira posição - medalha de bronze"
+              className="absolute top-0 right-8"
+            />
           </div>
         </div>
       </div>
