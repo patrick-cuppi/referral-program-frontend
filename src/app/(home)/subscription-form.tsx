ce3e35d812418c65d4fd9f5412cc5ec75a689e24
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-03-03 11:30:42 -0300

    :package: chore: react hook form

--- src/app/(home)/subscription-form.tsx
+++ src/app/(home)/subscription-form.tsx
@@ -1,10 +1,21 @@
+'use client'
+
 import { Button } from '@/components/button'
 import { InputField, InputIcon, InputRoot } from '@/components/input'
 import { ArrowRight, Mail, User } from 'lucide-react'
 
 export function SubscriptionForm() {
+  function onSubscribe(data: FormData) {
+    const values = Object.fromEntries(data.entries())
+
+    console.log(values)
+  }
+
   return (
-    <form className="bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[440px]">
+    <form
+      action={onSubscribe}
+      className="bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[440px]"
+    >
       <h2 className="font-heading font-semibold text-gray-200 text-xl">
         Inscrição
       </h2>
@@ -14,14 +25,14 @@ export function SubscriptionForm() {
           <InputIcon>
             <User />
           </InputIcon>
-          <InputField type="text" placeholder="Nome completo" />
+          <InputField name="name" type="text" placeholder="Nome completo" />
         </InputRoot>
 
         <InputRoot>
           <InputIcon>
             <Mail />
           </InputIcon>
-          <InputField type="email" placeholder="E-mail" />
+          <InputField name="email" type="email" placeholder="E-mail" />
         </InputRoot>
       </div>
 
